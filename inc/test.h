c5e564539025baf8430e2e4516ec3226d1f9f712
zhaobowen <bowen.zhao@greenstonesoft.com>
2023-07-24 17:37:17 +0800

    update

--- inc/test.h
+++ inc/test.h
@@ -5,6 +5,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <iostream>
 
 void epoll_test()
 {
@@ -14,11 +15,40 @@ void epoll_test()
     addr.sin_port = ntohs(2000);
 
     int udpsocket = socket(AF_INET,SOCK_DGRAM,IPPROTO_UDP);
-    struct epoll_event ep_event;
-    ep_event.events = EPOLLIN | EPOLLET;
+    if(bind(udpsocket,reinterpret_cast<sockaddr *>(&addr),sizeof(addr)) == -1)
+    {
+        std::cout<<"bind failed!"<<std::endl;
+        return;
+    }
+
+    struct epoll_event ep_event,ep_events[100];
+    ep_event.events = EPOLLIN;
+    ep_event.data.fd = udpsocket;
 
     int epoll_fd = epoll_create(100);
     epoll_ctl(epoll_fd,EPOLL_CTL_ADD,udpsocket,&ep_event);
+
+    while(true)
+    {
+        int nfds = epoll_wait(epoll_fd,ep_events,100,-1);
+        if( nfds!= -1)
+        {
+            for(int i = 0;i < nfds;++nfds)
+            {
+                char buffer[1000];
+                sockaddr_in client_addr;
+                socklen_t client_addr_len;
+                if(ep_events[i].events == EPOLLIN && ep_events[i].data.fd == udpsocket)
+                {
+                    recvfrom(ep_events[i].data.fd,buffer,100,0,reinterpret_cast<sockaddr*>(&client_addr),client_addr_len);
+                }
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
 }
 
 
